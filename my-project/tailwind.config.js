8f991a79a686fb4e1502f9a08393f24b312c3b1b
vitorpio7 <vitorvpio60@gmail.com>
2026-03-05 13:57:35 -0300

    feat:my component implemented

--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -1,14 +1,20 @@
 import withMT from "@material-tailwind/react/utils/withMT"
 
-export default withMT({
+export default {
+  content:[
+    "./src/**/*.{html,js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
         logo: ['Open Sans', 'sans-serif'],
         healing: ['Healing', 'sans-serif'],
       },
+      colors: {
+        primary: '#ff0000',
+        secondary: '#00ff00',
+      },
     },
   },
   plugins: [],
 }
-)
