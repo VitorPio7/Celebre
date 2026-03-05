946567649e5c79b52333e7430f3b00b7652c570e
vitorpio7 <vitorvpio60@gmail.com>
2026-03-05 14:59:02 -0300

    feat:my component implemented

--- my-project/src/components/BodyCarrossel.tsx
+++ my-project/src/components/BodyCarrossel.tsx
@@ -1,4 +1,8 @@
 import { CarouselTransition } from "./CarouselTransition";
+import { BsMoonStars } from "react-icons/bs";
+import { IoLeafOutline } from "react-icons/io5";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+
 export default function BodyCarrossel() {
   return (
     <div className="flex flex-col items-center justify-center text-center px-0 py-0 gap-0">
@@ -7,7 +11,23 @@ export default function BodyCarrossel() {
       </h1>
       <p>Transforme seu ambiente em um espaço único e personalizadas.</p>
       <CarouselTransition />
-      <div></div>
+      <div className="flex flex-row h-64 w-full justify-around">
+        <div className="flex flex-col items-center justify-center text-center px-0 py-0 gap-0">
+          <BsMoonStars />
+          <p>Personalize o seu evento</p>
+          <p>personalize o seu evento com luzes e cores</p>
+        </div>
+        <div className="flex flex-col items-center justify-center text-center px-0 py-0 gap-0">
+          <IoLeafOutline />
+          <p>Personalize o seu evento</p>
+          <p>personalize o seu evento com luzes e cores</p>
+        </div>
+        <div className="flex flex-col items-center justify-center text-center px-0 py-0 gap-0">
+          <HiOutlineLocationMarker />
+          <p>Personalize o seu evento</p>
+          <p>personalize o seu evento com luzes e cores</p>
+        </div>
+      </div>
     </div>
   );
 }
--- my-project/src/components/CarouselTransition.tsx
+++ my-project/src/components/CarouselTransition.tsx
@@ -1,23 +1,32 @@
-import { Carousel,  } from "@material-tailwind/react";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 export function CarouselTransition() {
   return (
-    <Carousel className="rounded-xl">
-      <img
-        src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-        alt="image 1"
-        className="h-full w-full object-cover"
-      />
-      <img
-        src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-        alt="image 2"
-        className="h-full w-full object-cover"
-      />
-      <img
-        src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
-        alt="image 3"
-        className="h-full w-full object-cover"
-      />
+    <Carousel className="w-full sm:max-w-xl md:max-w-4xl">
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index} className="basis-1/2 lg:basis-1/3">
+            <Card className="border-none shadow-none">
+              <CardContent className="p-0">
+                <img
+                  src={`image${index + 3}.jpeg`}
+                  className="w-full h-48 object-cover rounded-xl"
+                />
+              </CardContent>
+            </Card>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+
+      <CarouselPrevious />
+      <CarouselNext />
     </Carousel>
   );
 }
